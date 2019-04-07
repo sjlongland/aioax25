5bc90807afdd6c0818811eaa3b0d652c0fdd9161
Stuart Longland <me@vk4msl.id.au>
2019-04-07 20:50:02 +1000

    setup.py: Add set-up installer

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from aioax25 import __version__
+
+requirements = [
+        'pyserial',
+        'signalslot',
+]
+
+setup(
+        name='aioax25',
+        url='https://github.com/sjlongland/aioax25/',
+        version=__version__,
+        author='Stuart Longland VK4MSL',
+        author_email='me@vk4msl.id.au',
+        license='GPL-2.0-or-later',
+        packages=[
+            'aioax25',
+        ],
+        requires=requirements,
+        install_requires=requirements
+)
