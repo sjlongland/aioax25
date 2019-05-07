40e1318b9dd0fd6b719188bd9f0dbd7d09b78097
Stuart Longland <me@vk4msl.id.au>
2019-05-07 20:56:30 +1000

    test suite: Add binary comparison function

--- tests/hex.py
+++ tests/hex.py
@@ -14,3 +14,24 @@ def to_hex(bytestr):
         '%02x' % byte
         for byte in bytestr
     ])
+
+def hex_cmp(bytestr1, bytestr2):
+    if not isinstance(bytestr1, bytes):
+        bytestr1 = from_hex(bytestr1)
+
+    if not isinstance(bytestr2, bytes):
+        bytestr2 = from_hex(bytestr2)
+
+    assert bytestr1 == bytestr2, \
+            'Byte strings do not match:\n'\
+            '  1> %s\n'\
+            '  2> %s\n'\
+            '1^2> %s' % (to_hex(bytestr1),
+                    to_hex(bytestr2),
+                    ' '.join([
+                        (('%02x' % (a ^ b))
+                            if a != b
+                            else '  ')
+                        for (a, b)
+                        in zip(bytestr1, bytestr2)
+                    ]))
