d7d3cdafe632c8ee95cb87c0796c3029f02c8d27
Stuart Longland <me@vk4msl.id.au>
2019-05-07 20:57:56 +1000

    frame.AX25FrameHeader: Add some encoding tests.

--- tests/test_frame/test_ax25frameheader.py
+++ tests/test_frame/test_ax25frameheader.py
@@ -2,6 +2,7 @@
 
 from aioax25.frame import AX25Address, AX25FrameHeader
 from nose.tools import eq_
+from ..hex import from_hex, hex_cmp
 
 
 def test_decode_incomplete():
@@ -10,7 +11,9 @@ def test_decode_incomplete():
     """
     try:
         AX25FrameHeader.decode(
-                b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
+                from_hex(
+                    'ac 96 68 84 ae 92 e0'  # Destination
+                )
         )
         assert False, 'This should not have worked'
     except ValueError as e:
@@ -21,9 +24,10 @@ def test_decode_no_digis():
     Test we can decode an AX.25 frame without digipeaters.
     """
     (header, data) = AX25FrameHeader.decode(
-            b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
-            b'\xac\x96\x68\x9a\xa6\x98\x61' # Source
-            b'frame data goes here'         # Frame data
+            from_hex(
+                'ac 96 68 84 ae 92 e0'      # Destination
+                'ac 96 68 9a a6 98 61'      # Source
+            ) + b'frame data goes here'     # Frame data
     )
     eq_(header.destination, AX25Address('VK4BWI', ch=True))
     eq_(header.source, AX25Address('VK4MSL', extension=True))
@@ -35,10 +39,11 @@ def test_decode_with_1digi():
     Test we can decode an AX.25 frame with one digipeater.
     """
     (header, data) = AX25FrameHeader.decode(
-            b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
-            b'\xac\x96\x68\x9a\xa6\x98\x60' # Source
-            b'\xac\x96\x68\xa4\xb4\x84\x61' # Digi
-            b'frame data goes here'         # Frame data
+            from_hex(
+                'ac 96 68 84 ae 92 e0'          # Destination
+                'ac 96 68 9a a6 98 60'          # Source
+                'ac 96 68 a4 b4 84 61'          # Digi
+            ) + b'frame data goes here'         # Frame data
     )
     eq_(header.destination, AX25Address('VK4BWI', ch=True))
     eq_(header.source, AX25Address('VK4MSL'))
@@ -50,11 +55,12 @@ def test_decode_with_2digis():
     Test we can decode an AX.25 frame with two digipeaters.
     """
     (header, data) = AX25FrameHeader.decode(
-            b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
-            b'\xac\x96\x68\x9a\xa6\x98\x60' # Source
-            b'\xac\x96\x68\xa4\xb4\x84\x60' # Digi
-            b'\xac\x96\x68\xa4\xb4\x82\x61' # Digi
-            b'frame data goes here'         # Frame data
+            from_hex(
+                'ac 96 68 84 ae 92 e0'          # Destination
+                'ac 96 68 9a a6 98 60'          # Source
+                'ac 96 68 a4 b4 84 60'          # Digi 1
+                'ac 96 68 a4 b4 82 61'          # Digi 2
+            ) + b'frame data goes here'         # Frame data
     )
     eq_(header.destination, AX25Address('VK4BWI', ch=True))
     eq_(header.source, AX25Address('VK4MSL'))
@@ -62,3 +68,38 @@ def test_decode_with_2digis():
     eq_(header.repeaters[0], AX25Address('VK4RZB'))
     eq_(header.repeaters[1], AX25Address('VK4RZA', extension=True))
     eq_(data, b'frame data goes here')
+
+def test_encode_no_digis():
+    """
+    Test we can encode an AX.25 frame without digipeaters.
+    """
+    header = AX25FrameHeader(destination='VK4BWI', source='VK4MSL', cr=True)
+    hex_cmp(bytes(header),
+            'ac 96 68 84 ae 92 e0 '         # Destination
+            'ac 96 68 9a a6 98 61'          # Source
+    )
+
+def test_encode_1digi():
+    """
+    Test we can encode an AX.25 frame with one digipeater.
+    """
+    header = AX25FrameHeader(destination='VK4BWI', source='VK4MSL',
+            repeaters=('VK4RZB',), cr=True)
+    hex_cmp(bytes(header),
+            'ac 96 68 84 ae 92 e0 '         # Destination
+            'ac 96 68 9a a6 98 60 '         # Source
+            'ac 96 68 a4 b4 84 61'          # Digi
+    )
+
+def test_encode_2digis():
+    """
+    Test we can encode an AX.25 frame with two digipeaters.
+    """
+    header = AX25FrameHeader(destination='VK4BWI', source='VK4MSL',
+            repeaters=('VK4RZB', 'VK4RZA'), cr=True)
+    hex_cmp(bytes(header),
+            'ac 96 68 84 ae 92 e0 '         # Destination
+            'ac 96 68 9a a6 98 60 '         # Source
+            'ac 96 68 a4 b4 84 60 '         # Digi 1
+            'ac 96 68 a4 b4 82 61'          # Digi 2
+    )
