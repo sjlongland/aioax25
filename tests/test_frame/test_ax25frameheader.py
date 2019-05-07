9b8bebf649568fd4c78f80344fc818002175ca87
Stuart Longland <me@vk4msl.id.au>
2019-05-07 18:32:30 +1000

    frame.AX25FrameHeader tests: Add some decode tests.

---
+++ tests/test_frame/test_ax25frameheader.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from aioax25.frame import AX25Address, AX25FrameHeader
+from nose.tools import eq_
+
+
+def test_decode_no_digis():
+    """
+    Test we can decode an AX.25 frame without digipeaters.
+    """
+    (header, data) = AX25FrameHeader.decode(
+            b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
+            b'\xac\x96\x68\x9a\xa6\x98\x61' # Source
+            b'frame data goes here'         # Frame data
+    )
+    eq_(header.destination, AX25Address('VK4BWI', ch=True))
+    eq_(header.source, AX25Address('VK4MSL', extension=True))
+    eq_(data, b'frame data goes here')
+
+def test_decode_with_1digi():
+    """
+    Test we can decode an AX.25 frame with one digipeater.
+    """
+    (header, data) = AX25FrameHeader.decode(
+            b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
+            b'\xac\x96\x68\x9a\xa6\x98\x60' # Source
+            b'\xac\x96\x68\xa4\xb4\x84\x61' # Digi
+            b'frame data goes here'         # Frame data
+    )
+    eq_(header.destination, AX25Address('VK4BWI', ch=True))
+    eq_(header.source, AX25Address('VK4MSL'))
+    eq_(len(header.repeaters), 1)
+    eq_(header.repeaters[0], AX25Address('VK4RZB', extension=True))
+    eq_(data, b'frame data goes here')
+
+def test_decode_with_2digis():
+    """
+    Test we can decode an AX.25 frame with two digipeaters.
+    """
+    (header, data) = AX25FrameHeader.decode(
+            b'\xac\x96\x68\x84\xae\x92\xe0' # Destination
+            b'\xac\x96\x68\x9a\xa6\x98\x60' # Source
+            b'\xac\x96\x68\xa4\xb4\x84\x60' # Digi
+            b'\xac\x96\x68\xa4\xb4\x82\x61' # Digi
+            b'frame data goes here'         # Frame data
+    )
+    eq_(header.destination, AX25Address('VK4BWI', ch=True))
+    eq_(header.source, AX25Address('VK4MSL'))
+    eq_(len(header.repeaters), 2)
+    eq_(header.repeaters[0], AX25Address('VK4RZB'))
+    eq_(header.repeaters[1], AX25Address('VK4RZA', extension=True))
+    eq_(data, b'frame data goes here')
