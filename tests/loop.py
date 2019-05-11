f002ef4a3ea7d8383071a3e019ed9edae1945657
Stuart Longland <me@vk4msl.id.au>
2019-05-11 11:24:41 +1000

    Dummy loop: Return a time-out "handle" for unit tests.

--- tests/loop.py
+++ tests/loop.py
@@ -18,10 +18,30 @@ class DummyLoop(object):
         self.calls.append((self.time(), callback) + args)
 
     def call_later(self, delay, callback, *args):
-        self.calls.append((self.time() + delay, callback) + args)
+        when = self.time() + delay
+        self.calls.append((when, callback) + args)
+        return DummyTimeoutHandle(when)
 
     def add_reader(self, fileno, reader):
         self.readers[fileno] = reader
 
     def remove_reader(self, fileno):
         self.readers.pop(fileno)
+
+
+class DummyTimeoutHandle(object):
+    def __init__(self, when):
+        self._when = when
+        self._cancelled = False
+
+    def cancel(self):
+        self._cancelled = True
+
+    # Note: Don't rely on this in real code in Python <3.6 as
+    # these methods were added in 3.7.
+
+    def when(self):
+        return self._when
+
+    def cancelled(self):
+        return self._cancelled
