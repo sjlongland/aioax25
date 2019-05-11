e58e608d10002d5cd57c138dad038bf8301b4d51
Stuart Longland <me@vk4msl.id.au>
2019-05-11 11:12:35 +1000

    aprs.message tests: Add tests for message handler states.

--- tests/test_aprs/test_message.py
+++ tests/test_aprs/test_message.py
@@ -3,7 +3,7 @@
 import logging
 import gc
 
-from nose.tools import eq_, assert_greater
+from nose.tools import eq_, assert_greater, assert_is, assert_set_equal
 
 from aioax25.aprs.message import APRSMessageHandler, \
         APRSMessageAckFrame, APRSMessageRejFrame, APRSMessageFrame
@@ -30,6 +30,199 @@ class DummyAPRSHandler(object):
 
     def _on_msg_handler_finish(self, msgid):
         self.finished.append(msgid)
+
+
+def test_msghandler_enter_state_success():
+    """
+    Test the message considers 'SUCCESS' an exit state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.SUCCESS)
+
+    # 'done' signal should have been called.
+    eq_(len(calls),1)
+    call = calls.pop(0)
+
+    assert_set_equal(set(call.keys()), set(['handler', 'state']))
+    assert_is(call['handler'], msghandler)
+    eq_(call['state'], msghandler.HandlerState.SUCCESS)
+
+def test_msghandler_enter_state_reject():
+    """
+    Test the message considers 'REJECT' an exit state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.REJECT)
+
+    # 'done' signal should have been called.
+    eq_(len(calls),1)
+    call = calls.pop(0)
+
+    assert_set_equal(set(call.keys()), set(['handler', 'state']))
+    assert_is(call['handler'], msghandler)
+    eq_(call['state'], msghandler.HandlerState.REJECT)
+
+def test_msghandler_enter_state_timeout():
+    """
+    Test the message considers 'TIMEOUT' an exit state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.TIMEOUT)
+
+    # 'done' signal should have been called.
+    eq_(len(calls),1)
+    call = calls.pop(0)
+
+    assert_set_equal(set(call.keys()), set(['handler', 'state']))
+    assert_is(call['handler'], msghandler)
+    eq_(call['state'], msghandler.HandlerState.TIMEOUT)
+
+def test_msghandler_enter_state_cancel():
+    """
+    Test the message considers 'CANCEL' an exit state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.CANCEL)
+
+    # 'done' signal should have been called.
+    eq_(len(calls),1)
+    call = calls.pop(0)
+
+    assert_set_equal(set(call.keys()), set(['handler', 'state']))
+    assert_is(call['handler'], msghandler)
+    eq_(call['state'], msghandler.HandlerState.CANCEL)
+
+def test_msghandler_enter_state_reject():
+    """
+    Test the message considers 'REJECT' an exit state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.REJECT)
+
+    # 'done' signal should have been called.
+    eq_(len(calls),1)
+    call = calls.pop(0)
+
+    assert_set_equal(set(call.keys()), set(['handler', 'state']))
+    assert_is(call['handler'], msghandler)
+    eq_(call['state'], msghandler.HandlerState.REJECT)
+
+def test_msghandler_enter_state_send():
+    """
+    Test the message considers 'SEND' a intermediate state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.SEND)
+
+    # 'done' signal should not have been called.
+    eq_(len(calls),0)
+
+    # State should be reflected in the properties
+    eq_(msghandler.state, msghandler.HandlerState.SEND)
+
+def test_msghandler_enter_state_retry():
+    """
+    Test the message considers 'RETRY' a intermediate state.
+    """
+    calls = []
+    aprshandler = DummyAPRSHandler()
+    msghandler  = APRSMessageHandler(
+            aprshandler=aprshandler,
+            addressee='CQ',
+            path=['WIDE1-1','WIDE2-1'],
+            message='testing',
+            log=logging.getLogger('messagehandler'))
+    msghandler.done.connect(lambda **k : calls.append(k))
+
+    # Message handler is still in the INIT state
+    eq_(msghandler.state, msghandler.HandlerState.INIT)
+
+    # Tell it to go to the success state.
+    msghandler._enter_state(msghandler.HandlerState.RETRY)
+
+    # 'done' signal should not have been called.
+    eq_(len(calls),0)
+
+    # State should be reflected in the properties
+    eq_(msghandler.state, msghandler.HandlerState.RETRY)
 
 
 def test_msghandler_abort_on_no_aprshandler():
