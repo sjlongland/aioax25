fe4121f2f502634363fa26f5232747e4d6be14e7
Stuart Longland <me@vk4msl.id.au>
2019-05-12 12:00:32 +1000

    signal: Drop support for *args
    The base `signalslot.Slot` class does not support this.

--- aioax25/signal.py
+++ aioax25/signal.py
@@ -15,17 +15,15 @@ class Slot(BaseSlot):
     an exception handler.  It is the caller's responsibility to ensure the
     slot function handles its own exceptions.
     """
-    def __init__(self, slot_fn, *args, **kwargs):
+    def __init__(self, slot_fn, **kwargs):
         super(Slot, self).__init__(slot_fn)
-        self._slot_args = args
         self._slot_kwargs = kwargs
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, **kwargs):
         try:
-            call_args = self._slot_args + args
             call_kwargs = self._slot_kwargs.copy()
             call_kwargs.update(kwargs)
-            super(Slot, self).__call__(*call_args, **call_kwargs)
+            super(Slot, self).__call__(**call_kwargs)
         except:
             logging.getLogger(self.__class__.__module__).exception(
                     'Exception in slot %s', self.func
@@ -36,12 +34,12 @@ class OneshotSlot(Slot):
     """
     Helper class that calls a slot exactly once.
     """
-    def __init__(self, signal, *args, **kwargs):
+    def __init__(self, signal, slot_fn, **kwargs):
         self._signal = ref(signal)
-        super(OneshotSlot, self).__init__(*args, **kwargs)
+        super(OneshotSlot, self).__init__(slot_fn, **kwargs)
 
-    def __call__(self, *args, **kwargs):
-        super(OneshotSlot, self).__call__(*args, **kwargs)
+    def __call__(self, **kwargs):
+        super(OneshotSlot, self).__call__(**kwargs)
         signal = self._signal()
         if signal:
             signal.disconnect(self)
@@ -52,20 +50,20 @@ class Signal(BaseSignal):
     Wrap the `signalslot.Signal` so that *all* "slots" get called, regardless
     of whether they return something or not, or throw exceptions.
     """
-    def connect(self, slot, *args, **kwargs):
+    def connect(self, slot, **kwargs):
         """
         Connect a slot to the signal.  This will wrap the given slot up in
         an exception handler.  It is the caller's responsibility to ensure
         the slot handles its own exceptions.
         """
-        super(Signal, self).connect(Slot(slot, *args, **kwargs))
+        super(Signal, self).connect(Slot(slot, **kwargs))
 
-    def connect_oneshot(self, slot, *args, **kwargs):
+    def connect_oneshot(self, slot, **kwargs):
         """
         Connect a slot to the signal, and call it exactly once when the
         signal fires.  (Disconnect after calling.)
         """
-        super(Signal, self).connect(OneshotSlot(self, slot, *args, **kwargs))
+        super(Signal, self).connect(OneshotSlot(self, slot, **kwargs))
 
     def _find_slot(self, slot):
         """
