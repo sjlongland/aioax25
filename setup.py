ec486a758c6d2640d65bcbb8bffe6c316ca2ffa2
Stuart Longland <me@vk4msl.id.au>
2019-05-12 18:13:13 +1000

    setup.py: Add description, trove classifiers, etc.

--- setup.py
+++ setup.py
@@ -20,5 +20,21 @@ setup(
             'aioax25.aprs',
         ],
         requires=requirements,
-        install_requires=requirements
+        install_requires=requirements,
+        description='Asynchronous AX.25 interface in pure Python using asyncio',
+        long_description='aioax25 is a library for interfacing Python '\
+                'applications to AX.25 networks via serial KISS TNCs.  '\
+                'Right now it supports sending and receiving APRS messages '\
+                'and can handle multi-port KISS TNCs connected via serial '\
+                'ports or pseudo TTYs.',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: No Input/Output (Daemon)',
+            'Framework :: AsyncIO',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 3 :: Only',
+            'Topic :: Communications :: Ham Radio'
+        ]
 )
