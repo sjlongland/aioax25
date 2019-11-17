f46a300076e720b00064f6c19e3222acd8529d96
Stuart Longland <me@vk4msl.id.au>
2019-11-17 18:45:30 +1000

    station: Initial test cases

---
+++ tests/test_station/test_station.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from aioax25.station import AX25Station
+from aioax25.peer import AX25Peer
+from aioax25.version import AX25Version
+
+from nose.tools import eq_
+
+
+def test_constructor_log():
+    """
+    Test the AX25Constructor uses the log given.
+    """
+    class DummyInterface(object):
+        pass
+
+    class DummyLogger(object):
+        pass
+
+    log = DummyLogger()
+    interface = DummyInterface()
+    station = AX25Station(interface=interface, callsign='VK4MSL', ssid=3,
+            log=log)
+    assert station._log is log
+
+def test_constructor_loop():
+    """
+    Test the AX25Constructor uses the IO loop given.
+    """
+    class DummyInterface(object):
+        pass
+
+    class DummyLoop(object):
+        pass
+
+    loop = DummyLoop()
+    interface = DummyInterface()
+    station = AX25Station(interface=interface, callsign='VK4MSL', ssid=3,
+            loop=loop)
+    assert station._loop is loop
+
+def test_constructor_protocol():
+    """
+    Test the AX25Constructor validates the protocol
+    """
+    try:
+        class DummyInterface(object):
+            pass
+
+        AX25Station(interface=DummyInterface(), callsign='VK4MSL', ssid=3,
+                protocol=AX25Version.AX25_10)
+        assert False, 'Should not have worked'
+    except ValueError as e:
+        eq_(str(e),
+                "'1.x' not a supported AX.25 protocol version")
