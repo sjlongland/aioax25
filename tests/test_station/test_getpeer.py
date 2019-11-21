4185ba457691f946bf0e75bf8a926cbe16fe27ee
Stuart Longland <me@vk4msl.id.au>
2019-11-21 20:57:32 +1000

    station: Add existing peer retrieval test.

--- tests/test_station/test_getpeer.py
+++ tests/test_station/test_getpeer.py
@@ -2,9 +2,10 @@
 
 from aioax25.station import AX25Station
 from aioax25.peer import AX25Peer
+from aioax25.frame import AX25Address
 
 from nose.tools import eq_
-from ..mocks import DummyInterface
+from ..mocks import DummyInterface, DummyPeer
 
 
 def test_unknown_peer_nocreate_keyerror():
@@ -19,6 +20,7 @@ def test_unknown_peer_nocreate_keyerror():
         eq_(str(e), 'AX25Address(callsign=VK4BWI, ssid=0, '\
                 'ch=False, res0=True, res1=True, extension=False)')
 
+
 def test_unknown_peer_create_instance():
     """
     Test fetching an unknown peer with create=True generates peer
@@ -26,3 +28,18 @@ def test_unknown_peer_create_instance():
     station = AX25Station(interface=DummyInterface(), callsign='VK4MSL-5')
     peer = station.getpeer('VK4BWI', create=True)
     assert isinstance(peer, AX25Peer)
+
+
+def test_known_peer_fetch_instance():
+    """
+    Test fetching an known peer returns that known peer
+    """
+    station = AX25Station(interface=DummyInterface(), callsign='VK4MSL-5')
+    mypeer = DummyPeer(AX25Address('VK4BWI'))
+
+    # Inject the peer
+    station._peers[mypeer._address] = mypeer
+
+    # Retrieve the peer instance
+    peer = station.getpeer('VK4BWI')
+    assert peer is mypeer
