28538216dcc322d6f11d6cf95ba88d5e3b2b9873
Stuart Longland <me@vk4msl.id.au>
2019-11-21 20:57:49 +1000

    station: Add _drop_peer test

---
+++ tests/test_station/test_droppeer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from aioax25.station import AX25Station
+from aioax25.frame import AX25Address
+
+from nose.tools import eq_
+from ..mocks import DummyInterface, DummyPeer
+
+
+def test_known_peer_fetch_instance():
+    """
+    Test calling _drop_peer removes the peer
+    """
+    station = AX25Station(interface=DummyInterface(), callsign='VK4MSL-5')
+    mypeer = DummyPeer(AX25Address('VK4BWI'))
+
+    # Inject the peer
+    station._peers[mypeer._address] = mypeer
+
+    # Drop the peer
+    peer = station._drop_peer(mypeer)
+    assert mypeer._address not in station._peers
+    assert mypeer.address_read
