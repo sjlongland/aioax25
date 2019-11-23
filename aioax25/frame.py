9694d2570b56c3ea30eb4ca3a16b55a9d1380d76
Stuart Longland <me@vk4msl.id.au>
2019-11-23 15:54:11 +1000

    frame: Add test cases for other XID parameters

--- aioax25/frame.py
+++ aioax25/frame.py
@@ -1862,8 +1862,14 @@ class AX25XIDIFieldLengthTransmitParameter(AX25XIDBigEndianParameter):
     PI = AX25XIDParameterIdentifier.IFieldLengthTransmit
 
 
+AX25XIDParameter.register(AX25XIDIFieldLengthTransmitParameter)
+
+
 class AX25XIDIFieldLengthReceiveParameter(AX25XIDBigEndianParameter):
     PI = AX25XIDParameterIdentifier.IFieldLengthReceive
+
+
+AX25XIDParameter.register(AX25XIDIFieldLengthReceiveParameter)
 
 
 class AX25XIDWindowSizeTransmitParameter(AX25XIDBigEndianParameter):
@@ -1871,17 +1877,29 @@ class AX25XIDWindowSizeTransmitParameter(AX25XIDBigEndianParameter):
     LENGTH = 1
 
 
+AX25XIDParameter.register(AX25XIDWindowSizeTransmitParameter)
+
+
 class AX25XIDWindowSizeReceiveParameter(AX25XIDBigEndianParameter):
     PI = AX25XIDParameterIdentifier.WindowSizeReceive
     LENGTH = 1
+
+
+AX25XIDParameter.register(AX25XIDWindowSizeReceiveParameter)
 
 
 class AX25XIDAcknowledgeTimerParameter(AX25XIDBigEndianParameter):
     PI = AX25XIDParameterIdentifier.AcknowledgeTimer
 
 
+AX25XIDParameter.register(AX25XIDAcknowledgeTimerParameter)
+
+
 class AX25XIDRetriesParameter(AX25XIDBigEndianParameter):
     PI = AX25XIDParameterIdentifier.Retries
+
+
+AX25XIDParameter.register(AX25XIDRetriesParameter)
 
 
 class AX25ExchangeIdentificationFrame(AX25UnnumberedFrame):
