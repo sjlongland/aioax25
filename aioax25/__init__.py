a329644d265739bf5f1d9f342181f92cac49be7c
Stuart Longland <me@vk4msl.id.au>
2021-05-18 14:31:21 +1000

    Release 0.0.10
    Add support for TCP KISS connections.

--- aioax25/__init__.py
+++ aioax25/__init__.py
@@ -3,5 +3,5 @@
 __author__ = 'Stuart Longland VK4MSL'
 __email__ = 'me@vk4msl.id.au'
 __license__ = 'GPL-2.0-or-later'
-__copyright__ = 'Copyright 2019, Stuart Longland'
-__version__ = '0.0.9'
+__copyright__ = 'Copyright 2021, Stuart Longland (and contributors)'
+__version__ = '0.0.10'
