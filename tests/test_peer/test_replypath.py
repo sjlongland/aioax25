a610bf2476cd9566fe30546985260be69a212bc7
Stuart Longland <me@vk4msl.id.au>
2021-11-14 10:31:59 +1000

    peer: Pick most frequently seen RX path if no TX paths rated.

--- tests/test_peer/test_replypath.py
+++ tests/test_peer/test_replypath.py
@@ -44,6 +44,7 @@ def test_peer_reply_path_predetermined():
 
     assert list(peer.reply_path) == [AX25Address('VK4RZB')]
 
+
 def test_peer_reply_path_weight_score():
     """
     Test reply_path tries to select the "best" scoring path.
@@ -61,6 +62,36 @@ def test_peer_reply_path_weight_score():
 
     # Inject path scores
     peer._tx_path_score = {
+            AX25Path(AX25Address('VK4RZB')): 2,
+            AX25Path(AX25Address('VK4RZA')): 1
+    }
+
+    assert list(peer.reply_path) == [AX25Address('VK4RZB')]
+
+    # We should also use this from now on:
+    assert list(peer._reply_path) == [AX25Address('VK4RZB')]
+
+
+def test_peer_reply_path_rx_count():
+    """
+    Test reply_path considers received paths if no rated TX path.
+    """
+    station = DummyStation(AX25Address('VK4MSL', ssid=1))
+    peer = TestingAX25Peer(
+            station=station,
+            address=AX25Address('VK4MSL'),
+            repeaters=None,
+            locked_path=False
+    )
+
+    # Ensure not pre-determined path is set
+    peer._reply_path = None
+
+    # Ensure empty TX path scores
+    peer._tx_path_score = {}
+
+    # Inject path counts
+    peer._rx_path_count = {
             AX25Path(AX25Address('VK4RZB')): 2,
             AX25Path(AX25Address('VK4RZA')): 1
     }
