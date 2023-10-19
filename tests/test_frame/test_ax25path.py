6b9d5da53303256469208aff8deb5e6183f21241
Stuart Longland <me@vk4msl.com>
2023-10-19 15:15:07 +1000

    frame unit tests: Retire `eq_` from nosetests

--- tests/test_frame/test_ax25path.py
+++ tests/test_frame/test_ax25path.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from aioax25.frame import AX25Address, AX25Path
-from ..nosecompat import eq_
 
 
 def test_decode():
@@ -9,9 +8,9 @@ def test_decode():
     Test given a list of strings, AX25Path decodes them.
     """
     path = AX25Path("VK4MSL", "VK4RZB", "VK4RZA")
-    eq_(path._path[0]._callsign, "VK4MSL")
-    eq_(path._path[1]._callsign, "VK4RZB")
-    eq_(path._path[2]._callsign, "VK4RZA")
+    assert path._path[0]._callsign == "VK4MSL"
+    assert path._path[1]._callsign == "VK4RZB"
+    assert path._path[2]._callsign == "VK4RZA"
 
 
 def test_addresses():
@@ -23,9 +22,9 @@ def test_addresses():
         AX25Address.decode("VK4RZB"),
         AX25Address.decode("VK4RZA"),
     )
-    eq_(path._path[0]._callsign, "VK4MSL")
-    eq_(path._path[1]._callsign, "VK4RZB")
-    eq_(path._path[2]._callsign, "VK4RZA")
+    assert path._path[0]._callsign == "VK4MSL"
+    assert path._path[1]._callsign == "VK4RZB"
+    assert path._path[2]._callsign == "VK4RZA"
 
 
 def test_str():
@@ -33,7 +32,7 @@ def test_str():
     Test we can return the canonical format for a repeater path.
     """
     path = AX25Path("VK4MSL", "VK4RZB", "VK4RZA")
-    eq_(str(path), "VK4MSL,VK4RZB,VK4RZA")
+    assert str(path) == "VK4MSL,VK4RZB,VK4RZA"
 
 
 def test_repr():
@@ -41,24 +40,21 @@ def test_repr():
     Test we can return the Python representation for a repeater path.
     """
     path = AX25Path("VK4MSL", "VK4RZB", "VK4RZA")
-    eq_(
-        repr(path),
-        (
-            "AX25Path("
-            "AX25Address("
-            "callsign=VK4MSL, ssid=0, ch=False, "
-            "res0=True, res1=True, extension=False"
-            "), "
-            "AX25Address("
-            "callsign=VK4RZB, ssid=0, ch=False, "
-            "res0=True, res1=True, extension=False"
-            "), "
-            "AX25Address("
-            "callsign=VK4RZA, ssid=0, ch=False, "
-            "res0=True, res1=True, extension=False"
-            ")"
-            ")"
-        ),
+    assert repr(path) == (
+        "AX25Path("
+        "AX25Address("
+        "callsign=VK4MSL, ssid=0, ch=False, "
+        "res0=True, res1=True, extension=False"
+        "), "
+        "AX25Address("
+        "callsign=VK4RZB, ssid=0, ch=False, "
+        "res0=True, res1=True, extension=False"
+        "), "
+        "AX25Address("
+        "callsign=VK4RZA, ssid=0, ch=False, "
+        "res0=True, res1=True, extension=False"
+        ")"
+        ")"
     )
 
 
@@ -68,7 +64,7 @@ def test_reply():
     """
     path = AX25Path("VK4MSL*", "VK4RZB*", "VK4RZA*")
     reply = path.reply
-    eq_(str(reply), "VK4RZA,VK4RZB,VK4MSL")
+    assert str(reply) == "VK4RZA,VK4RZB,VK4MSL"
 
 
 def test_reply_unused():
@@ -77,7 +73,7 @@ def test_reply_unused():
     """
     path = AX25Path("VK4MSL*", "VK4RZB*", "VK4RZA")
     reply = path.reply
-    eq_(str(reply), "VK4RZB,VK4MSL")
+    assert str(reply) == "VK4RZB,VK4MSL"
 
 
 def test_replace():
@@ -86,4 +82,4 @@ def test_replace():
     """
     path1 = AX25Path("WIDE2-2", "WIDE1-1")
     path2 = path1.replace("WIDE2-2", "VK4MSL*")
-    eq_(str(path2), "VK4MSL*,WIDE1-1")
+    assert str(path2) == "VK4MSL*,WIDE1-1"
